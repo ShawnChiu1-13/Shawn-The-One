00e99eae6e3936ba30b2fa86f8b219a503cc56cf
ShawnChiu1-13 <89962884+ShawnChiu1-13@users.noreply.github.com>
2021-09-18 00:54:06 +0800

    Create main.py

--- 3119009448/main.py
+++ 3119009448/main.py
@@ -6,7 +6,8 @@ import gensim
 import os
 import sys
 import time
-#相比于上次修改更高校
+
+#相比于上次修改更高效
 #文档分句，删除符号，保留中文
 def creat_sentence(file_data):
     file_sentence = []
@@ -20,5 +21,19 @@ def creat_sentence(file_data):
             file_sentence.append(s)
             s = ''
     return file_sentence
+
+def tfidf_model(items,sim_items):
+    #生成词典
+    dictionary = corpora.Dictionary(items)
+    #生成稀疏向量库
+    corpus = [dictionary.doc2bow(text) for text in items]
+    #利用TFIDF模型建模
+    tf = models.TfidfModel(corpus)
+    #通过token2id得到特征数（字典里面的键的个数）
+    num_features = len(dictionary.token2id.keys())
+    #建立索引
+    index = similarities.MatrixSimilarity(tf[corpus],num_features=num_features)
+    #索引持久性
+    index.save('index.txt')
 
 
